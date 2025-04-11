ba6957051f1d9cc2b6ec1097c3896b78e7747a21
Madeline Prelee <mprelee@gmail.com>
2025-04-11 17:56:10 -0500

    ehllo world

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,15 @@ pub use ui::*;
 pub mod wasm;   
 pub use wasm::*;
 
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn greet() -> String {
+    String::from("Hello from WASM!")
+}
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
